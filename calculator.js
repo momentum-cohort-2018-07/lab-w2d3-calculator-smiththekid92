79977acd2598bee570698728478fbfe25a934987
PhoenixSmith <phoenix.smith.bcbsnc@gmail.com>
2018-08-08 18:15:20 -0400

    update JS to have calc functions display in console

--- calculator.js
+++ calculator.js
@@ -1,4 +1,82 @@
-var button = document.querySelector('.calc_input');
-button.addEventListener('click', function(){
-  return KeyboardEvent
+//can use debugger in place of break
+//at least get something to run in console
+
+var seven = document.querySelector('.calc_input7');
+seven.addEventListener('click', function(){
+    console.log('7')
+})
+
+var eight = document.querySelector('.calc_input8');
+eight.addEventListener('click', function(){
+    console.log('8')
+})
+
+var nine = document.querySelector('.calc_input9');
+nine.addEventListener('click', function(){
+    console.log('9')
+})
+
+var div = document.querySelector('.operationsdiv');
+div.addEventListener('click', function(){
+    console.log('/')
+})
+
+var four = document.querySelector('.calc_input4');
+four.addEventListener('click', function(){
+    console.log('4')
+})
+
+var five = document.querySelector('.calc_input5');
+five.addEventListener('click', function(){
+    console.log('5')
+})
+
+var six = document.querySelector('.calc_input6');
+six.addEventListener('click', function(){
+    console.log('6')
+})
+
+var multi = document.querySelector('.operationsmulti');
+multi.addEventListener('click', function(){
+    console.log('X')
+})
+
+var one = document.querySelector('.calc_input1');
+one.addEventListener('click', function(){
+    console.log('1')
+})
+
+var two = document.querySelector('.calc_input2');
+two.addEventListener('click', function(){
+    console.log('2')
+})
+
+var three = document.querySelector('.calc_input3');
+three.addEventListener('click', function(){
+    console.log('3')
+})
+
+var sub = document.querySelector('.operationssub');
+sub.addEventListener('click', function(){
+    console.log('-')
+})
+
+var zero = document.querySelector('.calc_input0');
+zero.addEventListener('click', function(){
+    console.log('0')
+})
+
+var dot = document.querySelector('.calc_inputdot');
+dot.addEventListener('click', function(){
+    console.log('.')
+})
+
+var equ = document.querySelector('.calc_inputequal');
+equ.addEventListener('click', function(){
+    console.log('=')
+})
+
+var plus = document.querySelector('.operationsplus');
+plus.addEventListener('click', function(){
+    console.log('=')
 })
